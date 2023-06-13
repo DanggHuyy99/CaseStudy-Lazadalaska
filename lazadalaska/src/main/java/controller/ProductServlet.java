944c60aaeebf8d12045188d505f855ea5caea7b7
chaunghia1992 <nchau0992@gmail.com>
2023-06-13 11:51:59 +0700

    doan van

--- lazadalaska/src/main/java/controller/ProductServlet.java
+++ lazadalaska/src/main/java/controller/ProductServlet.java
@@ -12,6 +12,7 @@ public class ProductServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doGet(req, resp);
+
     }
 
     @Override
